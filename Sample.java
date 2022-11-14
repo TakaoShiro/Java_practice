aff5841e2afe99b03590fcf5a2d17f823c9e055e
EC2 Default User <goemon.takao.75@gmail.com>
2022-11-14 13:00:54 +0000

    [add]java_practice

---
+++ Sample.java
@@ -0,0 +1,8 @@
+public class Sample {
+  public static void main(String[] args) {
+    String a = "文字列";
+    int b = 30;
+    System.out.println(a);
+    System.out.println(++b + 50);
+  }
+}
--- sample.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-    int a = 30;
-    int b = 30;
-    System.out.println(++a + 50);
-    System.out.println(b++ + 50);
-  }
-}
